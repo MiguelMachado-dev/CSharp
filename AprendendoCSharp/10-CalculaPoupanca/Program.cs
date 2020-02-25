d4c618e85903111e3e401d06215350979c4ad3f5
Miguel Machado <miiguellmp@gmail.com>
2020-02-25 03:38:33 -0300

    Usando While

---
+++ AprendendoCSharp/10-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando projeto 10...");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                // 0.36% = 0.36 / 100 = 0.0036
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine($"Apos {contadorMes} mes(es) voce tera R${valorInvestido}");
+
+                contadorMes++; // contadorMes = contadorMes + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
