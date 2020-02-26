bca11621aa3b4464bd47059bb1aadfbb66696089
Miguel Machado <miiguellmp@gmail.com>
2020-02-26 18:27:08 -0300

    Criando metodo para depositar e function para sacar

---
+++ ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,27 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    // Retorna valor = function
+    public bool Sacar(double valor) // Esta funcao retorna um boolean
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    // Nao retorna = metodo
+    public void Depositar(double valor) // void -> function nao tem retorno, nao devolve nada
+    {
+        this.saldo += valor;
+    }
+}
---
+++ ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Miguel";
+
+            Console.WriteLine($"Saldo atual: {conta.saldo}");
+            conta.Sacar(50);
+            Console.WriteLine($"Saldo apos saque: {conta.saldo}");
+
+            conta.Depositar(500);
+            Console.WriteLine($"Saldo apos deposito: {conta.saldo}");
+
+            Console.ReadLine();
+        }
+    }
+}
