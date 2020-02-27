eb791a14cef3143ba4ac0cc692221ad7114746f0
Miguel Machado <miiguellmp@gmail.com>
2020-02-26 22:49:33 -0300

    getters and setters

---
+++ ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Miguel Machado";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Titular = cliente;
+
+            conta.Saldo = 20;
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine($"Saldo da conta: {conta.Saldo}");
+
+            Console.ReadLine();
+        }
+    }
+}
