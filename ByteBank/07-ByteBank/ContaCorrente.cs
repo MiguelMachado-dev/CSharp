ae25356a52ea0da07a125d958df93a7e30bd9dcf
Miguel Machado <miiguellmp@gmail.com>
2020-02-26 23:00:33 -0300

    construtores para evitar problemas no codigo

---
+++ ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        // Propriedades
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0) // value eh o nome do argumento que vem no set
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        // Retorna valor = function
+        public bool Sacar(double valor) // Esta funcao retorna um boolean
+        {
+            if (_saldo < valor)
+            {
+                Console.WriteLine("Valor nao permitido para saque");
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+
+        }
+
+        // Nao retorna = metodo
+        public void Depositar(double valor) // void -> function nao tem retorno, nao devolve nada
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
