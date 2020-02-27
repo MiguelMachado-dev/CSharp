45610d96ac92c8816f2d1786ded6c6fcb575c23e
Miguel Machado <miiguellmp@gmail.com>
2020-02-26 22:00:50 -0300

    Instanciando novo obj dentro de um metodo

--- ByteBank/05-ByteBank/Program.cs
+++ ByteBank/05-ByteBank/Program.cs
@@ -7,14 +7,20 @@ namespace _05_ByteBank
         static void Main(string[] args)
         {
 
-            Cliente miguel = new Cliente();
+            //Cliente miguel = new Cliente();
 
-            miguel.nome = "Miguel Machado";
-            miguel.profissao = "FullStack Developer";
+            //miguel.nome = "Miguel Machado";
+            //miguel.profissao = "FullStack Developer";
 
             ContaCorrente conta = new ContaCorrente();
+            conta.titular = new Cliente();
+            conta.titular.nome = "Miguel Machado";
+            conta.titular.cpf = "123.456.789-01";
+            conta.titular.profissao = "FullStack Developer";
+            conta.agencia = 0001;
+            conta.numero = 1285865;
 
-            conta.titular = miguel;
+            //conta.titular = miguel;
             Console.WriteLine(conta.titular.nome);
 
             Console.ReadLine();
