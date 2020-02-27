1262551bcec2a261d653a5c967faac7cbf256c51
Miguel Machado <miiguellmp@gmail.com>
2020-02-26 21:57:22 -0300

    Composicao de Classes

---
+++ ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        // Retorna valor = function
+        public bool Sacar(double valor) // Esta funcao retorna um boolean
+        {
+            if (this.saldo < valor)
+            {
+                Console.WriteLine("Valor nao permitido para saque");
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+
+        }
+
+        // Nao retorna = metodo
+        public void Depositar(double valor) // void -> function nao tem retorno, nao devolve nada
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente miguel = new Cliente();
+
+            miguel.nome = "Miguel Machado";
+            miguel.profissao = "FullStack Developer";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = miguel;
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
