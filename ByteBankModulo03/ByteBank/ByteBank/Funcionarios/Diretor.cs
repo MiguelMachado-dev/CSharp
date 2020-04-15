57c249f8b21c0ee1ec6f1fa60207aca09e9f198e
Miguel Machado <miiguellmp@gmail.com>
2020-04-15 16:21:04 -0300

    Sobrepondo comportamentos padrao de herança com override e virtual

--- ByteBankModulo03/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ ByteBankModulo03/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -8,7 +8,7 @@ namespace ByteBank.Funcionarios
 {
     public class Diretor : Funcionario
     {
-        public new double GetBonificacao()
+        public override double GetBonificacao()
         {
             return Salario;
         }
